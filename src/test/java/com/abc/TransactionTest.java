304129163a7f18e0e85018091f35ad0f404f482f
Andrei Holub <aholub@ipsoft.com>
2015-08-31 20:53:18 -0400

    meaningful test for Transactions

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -3,13 +3,17 @@ package com.abc;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 import static org.junit.Assert.assertTrue;
 
 public class TransactionTest {
+
     @Test
-    public void transaction() {
+    public void testTransactionDate() {
         Transaction t = new Transaction(new BigDecimal("5"));
-        assertTrue(t instanceof Transaction);
+
+        // XXX: No nanos for Date(). Dates can be equal.
+        assertTrue(t.getTransactionDate().compareTo(new Date()) <= 0);
     }
 }
